b5e7f7559672171c84c6f7cef57c5224d39422a2
Jennifer Lara <jenniferlara@Jennifers-MacBook-Pro.local>
2017-07-24 17:14:00 -0400

    added JAWSDB info

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,22 @@
+//requiring mysql package
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+var connection;
 
-connection.connect(function(err) {
-	if (err) {
-		console.error('error connecting: ' + err.stack);
-		return;
-	}
-		console.log('connected as id ' + connection.threadId);
-})
+if (process.env.JAWSDB_URL) {
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	// DB is local on localhost
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: '',
+		database: 'burgers_db'
+	});
+};
+
+connection.connect();
 
 module.exports = connection;
